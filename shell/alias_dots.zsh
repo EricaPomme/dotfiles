6069cee1a5d8519901a475d785c5fec9802ec2c2
Erica Young <git@pomme.cafe>
2025-06-20 12:02:13 -0700

    Fix typo in alias_dots and add trailing newline

--- shell/alias_dots.zsh
+++ shell/alias_dots.zsh
@@ -1,5 +1,5 @@
 # Alias to change '..' to 'cd ..' and beyond.
-# Change {2..x} to add more dots but 10 is probably alredy stupid :3
+# Change {2..x} to add more dots but 10 is probably already stupid :3
 
 for i in {2..10}; do
     alias "$(printf '.%.0s' $(seq 1 $i))"="cd $(printf '../%.0s' $(seq 1 $i))"
